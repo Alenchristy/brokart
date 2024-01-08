509fb67bf80bba02640edf74187405e6b0021a8a
Alenchristy <alenchristy0201@gmail.com>
2024-01-08 16:03:57 +0530

    third commit

--- brokart/customers/urls.py
+++ brokart/customers/urls.py
@@ -5,7 +5,5 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('product_list',views.list_products,name='list_product'),
-    path('product_details',views.detail_product,name='detail_product')
+    path('account',views.show_account,name='account')
 ]
