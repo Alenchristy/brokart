2c0c038429808173473ee9a99deed75b51d4f943
Alenchristy <alenchristy0201@gmail.com>
2024-01-16 17:30:58 +0530

    fourth commit

--- brokart/brokart/urls.py
+++ brokart/brokart/urls.py
@@ -24,6 +24,5 @@ urlpatterns = [
     path('',include('products.urls')),
     path('customer',include('customers.urls')),
     path('orders',include('orders.urls')),
-
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
