00b2d0de19c692f603474f581e3960bbfc3f424a
Alenchristy <alenchristy0201@gmail.com>
2024-01-17 15:30:48 +0530

    fourth commit

--- brokart/customers/views.py
+++ brokart/customers/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
 from . models import Customer
 from django.contrib import messages
 # Create your views here.
+def sign_out(request):
+    logout(request)
+    return redirect('home')
+
 
 def show_account(request):
+    context = {}
     if request.POST and 'register' in request.POST:
+        context['register']=True
         try:
             username=request.POST.get('username')
             password=request.POST.get('password')
@@ -13,7 +20,7 @@ def show_account(request):
             address=request.POST.get('address')
             phone=request.POST.get('phone')
             # create user account
-            user=User.objects.create(
+            user=User.objects.create_user(
                 username=username,
                 password=password,
                 email=email
@@ -24,8 +31,20 @@ def show_account(request):
                 phone=phone,
                 address=address
             )
-            return redirect('home')
+            success_message="User registerd successfully"
+            messages.success(request,success_message)
+            context
         except Exception as e:
-            error_message="Duplicate Credentials"
+            error_message="Duplicate Inputs"
             messages.error(request,error_message)
-    return render(request,'account.html')
+    if request.POST and 'login' in request.POST:
+        context['register']=False
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(username=username,password=password)
+        if user:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,'invalid user credentials')
+    return render(request,'account.html',context)
